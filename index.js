8a2e6d3a54857f9476c4e53e79abe1b8f857abb9
DevRefaul <devrefaul@gmail.com>
2022-11-25 22:20:22 +0600

    adding user api created and working fine

--- index.js
+++ index.js
@@ -1,25 +1,111 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config()
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken')
+const { MongoClient, ObjectId } = require('mongodb');
 const app = express()
-
 
 
 app.use(cors())
 app.use(express.json())
 
 
-app.get('/', (req, res) => {
-    res.send('Server is up and Running')
-})
+// uri of mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ag5u5eg.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri)
+
+// main apis function
+
+const run = async () => {
+
+    const Bikes = client.db('USB').collection('bikes')
+    const users = client.db('USB').collection('users')
+
+    try {
+
+        // basic root api
+        app.get('/', (req, res) => {
+            res.send('Server is up and Running')
+        })
 
 
-app.get('/bikes', (req, res) => {
-    const bikes = require('./bikesDetail.json')
-    res.send({ bikes })
-})
+        // getting bikes by category
+        app.get('/bikes', async (req, res) => {
+            const query = req.query;
+
+            // filtering the data
+            const filter = query
+            // getting the values
+            const bikes = await Bikes.find(filter).toArray()
+            res.send({ bikes })
+        })
+
+        // getting bike by id
+        app.get('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+
+            // filtering the data
+            const query = { _id: ObjectId(id) }
+            // getting the values
+            const bike = await Bikes.findOne(query)
+            res.send({ bike })
+        })
 
 
+        // creating user collection in database
+        app.post('/adduser', async (req, res) => {
+
+            try {
+                const user = req.body;
+                console.log(user)
+                const result = await users.insertOne(user)
+                res.send({
+                    message: "Success",
+                    result
+                })
+            } catch (error) {
+                res.status(401).send({
+                    message: error.message
+                })
+            }
+
+
+        })
+
+
+        // getting a user by email
+        app.get('/user', async (req, res) => {
+            try {
+                const email = req.query.email;
+                const filter = { email: email }
+                const result = await users.findOne(filter)
+                res.send({
+                    message: "Success",
+                    result
+                })
+            } catch (error) {
+                res.status(401).send({
+                    message: error.message
+                })
+            }
+        })
+
+
+
+
+
+
+
+
+    } catch (error) {
+
+    }
+
+}
+
+run()
 
 
 
