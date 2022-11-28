4144e04a4ec1646a9914a5e697274f771c887441
DevRefaul <devrefaul@gmail.com>
2022-11-28 09:29:42 +0600

    payment api updated

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken')
 const { MongoClient, ObjectId } = require('mongodb');
 const app = express()
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")("sk_test_51M7I4PBM1N4t2PWxyTQ1eKolkTEiUUCpkWMKrx1UJDyyqz7huS1gSwy3EqzzDP1Yrn0gMuO79da0K0DUJuaUuUVB00vBlJ4y7g");
 
 app.use(cors())
 app.use(express.json())
-
 
 // uri of mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ag5u5eg.mongodb.net/?retryWrites=true&w=majority`;
@@ -38,16 +37,17 @@ const run = async () => {
         // payment api for user
         app.post("/create-payment-intent", async (req, res) => {
             try {
+
                 const price = req.body.price;
-                const amount = parseFloat(price * 100)
+                const amount = parseFloat(price)
 
                 // Create a PaymentIntent with the order amount and currency
                 const paymentIntent = await stripe.paymentIntents.create({
+                    currency: 'usd',
                     amount: amount,
-                    currency: "usd",
-                    automatic_payment_methods: {
-                        enabled: true,
-                    },
+                    "payment_method_types": [
+                        "card"
+                    ]
                 });
 
                 res.send({
@@ -217,11 +217,8 @@ const run = async () => {
             try {
                 const id = req.params.id;
                 const updatedInfo = req.body;
-                console.log(updatedInfo);
                 const filter = { _id: ObjectId(id) }
-                console.log(filter)
                 const updatedDoc = { $set: updatedInfo }
-                console.log(updatedDoc);
                 const updatedResponse = await Bikes.updateOne(filter, updatedDoc)
                 res.send({
                     message: "success",
